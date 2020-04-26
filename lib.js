dffdf6a663211f32d329a7034d5bc299f4fd5828
Anurag <anurag.kumar@feryservices.com>
2020-04-26 14:20:05 +0530

    refactoring with confidence

--- lib.js
+++ lib.js
@@ -3,7 +3,19 @@
 
 // Testing numbers
 module.exports.absolute = function (number) {
+  /* 
   if (number > 0) return number;
   if (number < 0) return -number;
-  return 0;
+  return 0; 
+  */
+
+  //First Refactoring
+  /* 
+  if (number >= 0) return number;
+
+  return -number; 
+  */
+
+  //Second Refactoring
+  return number >= 0 ? number : -number;
 };
--- tests/lib.test.js
+++ tests/lib.test.js
@@ -1,16 +1,18 @@
 const lib = require("../lib");
 
-test("absolute - should return a +ve number if input is +ve", () => {
-  const result = lib.absolute(1);
-  expect(result).toBe(1);
-});
+describe("absolute", () => {
+  it("should return a +ve number if input is +ve", () => {
+    const result = lib.absolute(1);
+    expect(result).toBe(1);
+  });
 
-test("absolute - should return a +ve number if input is -ve", () => {
-  const result = lib.absolute(-1);
-  expect(result).toBe(1);
-});
+  it("should return a +ve number if input is -ve", () => {
+    const result = lib.absolute(-1);
+    expect(result).toBe(1);
+  });
 
-test("absolute - should return 0 if input is 0", () => {
-  const result = lib.absolute(0);
-  expect(result).toBe(0);
+  it("should return 0 if input is 0", () => {
+    const result = lib.absolute(0);
+    expect(result).toBe(0);
+  });
 });
